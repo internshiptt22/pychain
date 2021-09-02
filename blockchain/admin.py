8e26c63238ac97fdc6d819915718f6c328dbf235
VIDYA KHARVI <73478592+vidyakharvi2@users.noreply.github.com>
2021-09-02 12:23:56 +0530

    changed the variables  names
    it is very easy to understand with proper variable nname.

--- blockchain/admin.py
+++ blockchain/admin.py
@@ -2,9 +2,8 @@ import networkx as nx
 
 
 print('Imported a network module')
-a = 10 
-b = 5 
-sum = a + b
-print(sum)
-
+val1 = 20;
+val2= 20;
+sum=val1+val2;
+print(sum);
 # Register your models here.
